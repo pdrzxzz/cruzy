824916a81f82d15eaa390b926f8d6d7b3b980031
Emanuel Pedroza <emanuelsalgado007@gmail.com>
2025-01-30 08:35:54 -0300

    initial database commit

--- routes/single.js
+++ routes/single.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
 
 /* GET single-player page. */
 router.get('/', (req, res, next) => {
   res.render('single/index');
+  main().catch(err => console.log(err));
+
+  async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/test');
+    console.log('DB Connected')
+  }
 });
 
 router.post('/', (req, res, next) => {
+  //check if the room can be created and redirect
   req.flash('success', 'New room created!');
   req.session.data = req.body;
   res.redirect('/play');
