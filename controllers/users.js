71a8fd55fe66339a82038aad05c18e5b73a56981
Emanuel Pedroza <emanuelsalgado007@gmail.com>
2025-02-10 09:10:09 -0300

    remove email registering

--- controllers/users.js
+++ controllers/users.js
@@ -6,8 +6,8 @@ module.exports.renderRegister = (req, res) => {
 
 module.exports.register = async (req, res) => {
     try {
-        const { email, username, password } = req.body
-        const user = new User({ email, username })
+        const { username, password } = req.body
+        const user = new User({ username })
         const registeredUser = await User.register(user, password)
         req.login(registeredUser, err => {
             if (err) return next(err)
--- models/user.js
+++ models/user.js
@@ -2,13 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const passportLocalMongoose = require('passport-local-mongoose');
 
-const UserSchema = new Schema({
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-    }
-})
+const UserSchema = new Schema({})
 UserSchema.plugin(passportLocalMongoose)
 
 module.exports = mongoose.model('User', UserSchema)
