dbdc91a242b79e2cfb149cb0fc4c69f41bde0942
Bianca Paes <160030245+bianca-bpas@users.noreply.github.com>
2025-02-18 14:10:32 +0000

    removendo o teste com sqlite

--- models/user.js
+++ models/user.js
@@ -1,41 +1,8 @@
-let db;
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const passportLocalMongoose = require('passport-local-mongoose');
 
-module.exports = {
-    // Injeta a instância do banco
-    setDB: function(database) {
-        db = database;
-    },
+const UserSchema = new Schema({})
+UserSchema.plugin(passportLocalMongoose)
 
-    register: async function(user, password) {
-        try {
-            const result = await db.run(
-                'INSERT INTO users (username, password) VALUES (?, ?)',
-                user.username,
-                password
-            );
-            return { id: result.lastID, username: user.username, password };
-        } catch (error) {
-            throw error;
-        }
-    },
-
-    authenticate: async function(username, password) {
-        const foundUser = await db.get('SELECT * FROM users WHERE username = ?', username);
-        if (!foundUser) {
-            throw new Error('User not found');
-        }
-        if (foundUser.password !== password) {
-            return { message: 'Incorrect password for username' };
-        }
-        return { user: foundUser };
-    },
-
-    updatePassword: async function(username, newPassword) {
-        await db.run('UPDATE users SET password = ? WHERE username = ?', newPassword, username);
-        return await db.get('SELECT * FROM users WHERE username = ?', username);
-    },
-
-    delete: async function(username) {
-        await db.run('DELETE FROM users WHERE username = ?', username);
-    }
-};
+module.exports = mongoose.model('User', UserSchema)
